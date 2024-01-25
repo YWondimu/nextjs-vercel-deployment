ab3321ab6d28224d778d84cb270f8d7f4951b2c2
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2024-01-25 18:24:50 -0400

    add nextjs config to use https

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const fs = require('fs');
+const https = require('https');
+
+const nextConfig = {
+    server: {
+        https: {
+            key: fs.readFileSync('./localhost.key'),
+            cert: fs.readFileSync('./localhost.crt'),
+        }
+    }
+};
 
 module.exports = nextConfig
