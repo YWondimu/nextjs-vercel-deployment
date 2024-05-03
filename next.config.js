0960fa36c973e9d6f14562a9388f35528279f709
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2024-05-03 10:48:47 -0300

    Made some changes, not sure what

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 
 const nextConfig = {
-    server: {
-        https: {
-            key: fs.readFileSync('./localhost.key'),
-            cert: fs.readFileSync('./localhost.crt'),
-        }
-    }
+    // server: {
+    //     https: {
+    //         key: fs.readFileSync('./localhost.key'),
+    //         cert: fs.readFileSync('./localhost.crt'),
+    //     }
+    // }
 };
 
 module.exports = nextConfig
