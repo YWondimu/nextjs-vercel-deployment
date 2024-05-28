2a70d582d3155156d47f5a6b4bc4f5be1517beb4
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2024-05-28 10:34:28 -0300

    Initial Middle.js

--- src/app/Middle.js
+++ src/app/Middle.js
@@ -1,9 +1,53 @@
 'use client'
+import { motion } from 'framer-motion';
+
+// TODO: fix the other fiels so that tehy are "zoomed" like b-zoomed is.
+// TODO: make it so that dragging the map slides it UNDER the top and bottom divs, not above
+// --> TODO: I can review stacking contexts, and I can look at the new pop or layout or whatever api.
+
+let folder ='/unb-floors-with-shadow/';
+// let folder ='/unb-floors/';
+let floorName = [
+  'b-zoomed',
+  'e',
+  'd',
+  'c',
+  'b',
+];
+let fileLocation = folder + 'floor-' + floorName[0] + '.svg';
+
+
+// public/next.svg
+// public/unb-floors/floor-b.svg
+// public/unb-floors/floor-c.svg
+// public/unb-floors/floor-d.svg
+// public/unb-floors/floor-e.svg
+// public/unb-floors/floor-f.svg
+// public/unb-floors-with-shadow/floor-b-zoomed.svg
+// public/unb-floors-with-shadow/floor-b.svg
+// public/unb-floors-with-shadow/floor-c.svg
+// public/unb-floors-with-shadow/floor-d.svg
+// public/unb-floors-with-shadow/floor-e.svg
+// public/unb-floors-with-shadow/floor-f.svg
+// public/vercel.svg
+
+// let fileLocation = '';
 
 export default function Middle() {
     return (
-        <div className='middle'>
-      hello 
-        </div>
+      <>
+      <div className="middle">
+        {fileLocation}
+        <motion.img
+            className='image' 
+            drag 
+            dragMomentum={false}
+            // animate={{scale: 1}}
+            src={fileLocation}
+            alt=''
+        >
+        </motion.img>
+      </div>
+      </>
     );
 }
