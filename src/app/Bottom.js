c02ea0a82057e092e8a7c9d72bd4ef2b4b1f4c2d
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2024-05-30 22:51:54 -0300

    Track taps, touches, drags
    WHAT WORKS PERFECTLY
    I can track touching, dragging, and single finger taps (of any
    number: single, double, triple, etc.).
    
    WHAT MOSTLY WORKS
    But for counting double-finger double taps (or triple taps or higher)
    of two fingers (not one) I had to use a method that seems to only work
    most of the time. I get 1.5 or 2.0 as a number for the number of taps
    when I double-tap with two fingers, and 2.5 or 3.0 when
    I triple tap (with two fingers).
    
    This is stil useful, because I just need to round up the number:
    round(1.5) == round(2.0) and round(2.5) == round(3.0).
    
    HOW OFTEN IT WORKS
    This works about ... 90% of the time? 95%? 99%?
    In any case, so far, I think it's good enough.
    
    TRIPLE FINGER TAPS: OUT OF SCOPE
    Triple finger taps don't work so well on my iphone, so... I'm going
    to give up on trying to implement them. I don't think it will be
    necessary given the current scope of the app.

--- src/app/Bottom.js
+++ src/app/Bottom.js
@@ -1,8 +1,98 @@
 'use client'
+import { useState, useEffect, useRef } from 'react';
+
 
 export default function Bottom() {
+    const MAX_TAP_DELAY = 200; // Maximum delay between taps in milliseconds
+    const MAX_TAP_INTERVAL = 200; // Maximum delay between taps in milliseconds
+
+    let lastTapTime = 0;
+    let tapTimeout;
+
+    const [text, setText] = useState('nada');
+    const [initialPosition, setInitialPosition] = useState({x: undefined, y: undefined});
+    const [position, setPosition] = useState(
+        //note: fill(null) needed for map to work apparently
+        new Array(5).fill(null).map( () => ({x: undefined, y: undefined}) )
+    );
+
+    const [numOfFingers, setNumOfFingers] = useState(0);
+    const [isTouching, setIsTouching] = useState(false);
+    const [isDragging, setIsDragging] = useState(false);
+    const [tapCount, setTapCount] = useState(0);
+    const [tapStart, setTapStart] = useState(0);
+    const [tapEnd, setTapEnd] = useState(new Date().getTime());
+    const [prevIsTap, setPrevIsTap] = useState(false);
+    const [tapInterval, setTapInterval] = useState(0);
+
+    const handleTouchStart = (e) => {
+        let numOfFingers = e.touches.length;
+        setNumOfFingers(numOfFingers);
+
+        setIsTouching(true);
+        let currentTime = new Date().getTime();
+        setTapStart(currentTime);
+        setTapInterval(currentTime - tapEnd);
+
+        const f = {x: e.touches[0].clientX, y: e.touches[0].clientY};
+        const touches = e.touches;
+        let text = '';
+        for (let i = 0; i < position.length; i++) {
+            position[i].x = e.touches[i]?.clientX;
+            text += 'x: ' + position[i].x + '\n';
+            position[i].y = e.touches[i]?.clientY;
+            text += 'y: ' + position[i].y + '\n';
+        }
+        //setText(text);
+        setPosition(position);
+    }
+
+    const handleTouchMove = (e) => {
+        setIsDragging(true);
+    }
+
+    const handleTouchEnd = (e) => {
+        setIsDragging(false);
+        setIsTouching(false);
+        let currentTime = new Date().getTime();
+        setTapEnd(new Date().getTime());
+        let tapDuration = currentTime - tapStart;
+
+        //if tapDuration too long --> reset tap count
+        if (tapDuration > MAX_TAP_DELAY) {
+            setPrevIsTap(false);
+            setTapCount(0);
+        //else if tapDuration not too long, and other finger not still touching --> add to tapCount
+        } else {
+            setPrevIsTap(true);
+            //if tapInterval too long, --> this is first tap
+            if (tapInterval > MAX_TAP_INTERVAL) {
+                setTapCount(1);
+            } else {
+                setText('num:' + numOfFingers);
+                //i think this works, but ... works only sometimes.
+                //it feels probabilistic. is there a better way?
+                if (numOfFingers > 1) {
+                    let delta = 1/numOfFingers;
+                    setTapCount( prev => prev+delta );
+                } else {
+                    setTapCount( prev => prev+1 );
+                }
+                //alert('Start-End: ' + currentTime + ' ' + tapEnd + ' ' + (currentTime-tapEnd));
+                //alert(text + tapStart + ' ' + currentTime + ' ' + (currentTime-tapStart));
+            }
+        }
+    }
+
     return (
-        <div className='bottom'>
+        <div className='bottom' onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
+            <p>{text}</p>
+            <p>0 (init) x: {position[0].x} y: {position[0].y} (cur) x: y: </p>
+            <p>num of fingers: {numOfFingers}</p>
+            <p>number of taps: {tapCount}</p>
+            <p>touch {''+isTouching} drag {''+isDragging}</p>
+            zoom scale: 
+            drag length: 
         </div>
     );
 }
