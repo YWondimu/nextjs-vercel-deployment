7fac7039dde81f4feedbd3536179a4201b26a188
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2024-07-11 17:41:12 -0300

    Add icons

--- src/app/Top.js
+++ src/app/Top.js
@@ -1,8 +1,163 @@
 'use client'
 import { useState } from 'react';
 import ToggleButton from './ToggleButton';
+//import {
+//    //washroom
+//    BiMaleFemale, 
+//    FaFemale, FaMale, BiFemale, BiMale,
+//    //food
+//    GrCafeteria,  IoCafe, LuGlassWater, MdWaterDrop, GiVendingMachine, PiBowlFoodFill, PiBowlFoodDuotone, IoFastFood, MdEmojiFood, 
+//    //exits
+//    FaStairs, MdOutlineStairs, MdStairs, Gi3dStairs, GiStairs,
+//    RxExit, ImExit, IoExit, 
+//    FaElevator, MdElevator, MdOutlineElevator, GrElevator,
+//    //seating areas
+//    MdChair, MdOutlineChair, PiChairBold, PiChair, 
+//    //accessibility
+//    PiWheelchairmotion,
+//    //roooms
+//    IoText,
+//    //pins
+//    IoMdPin, MdLoationPin, LuMapPin,
+//    //arrows (eg for exits, or pins)
+//    FaArrowLeft, FaArrowRight, FaArrowUp, FaArrowDown,
+//    FaLongArrowAltLeft, FaLongArrowAltRight, FaLongArrowAltUp, FaLongArrowAltDown,
+//
+//} from 'react-icons';
+//washroom
 
-export default function Top({ isPressed, setIsPressed }) {
+//washroom
+import { BiMaleFemale, BiFemale, BiMale } from 'react-icons/bi';
+import { FaFemale, FaMale, } from 'react-icons/bi';
+
+//food
+import { GrCafeteria } from 'react-icons/gr';
+import { IoCafe, IoFastFood } from 'react-icons/io5';
+import { LuGlassWater } from 'react-icons/lu';
+import { MdWaterDrop, MdEmojiFood } from 'react-icons/md';
+import { GiVendingMachine } from 'react-icons/gi';
+import { PiBowlFoodFill, PiBowlFoodDuotone, } from 'react-icons/gi';
+
+//exits
+import { FaStairs, FaElevator } from 'react-icons/fa6';
+import { MdOutlineStairs, MdStairs, MdElevator, MdOutlineElevator } from 'react-icons/md';
+import { GrElevator } from 'react-icons/gr';
+import { Gi3dStaris, GiStairs } from 'react-icons/gi';
+import { RxExit } from 'react-icons/rx';
+import { ImExit } from 'react-icons/im';
+import { IoExit } from 'react-icons/io';
+
+//seating areas
+import { MdChair, MdOutlineChair } from 'react-icons/md';
+import { PiChairBold, PiChair } from 'react-icons/pi';
+//accessibility
+import { PiWheelchairmotion } from 'react-icons/pi';
+//roooms
+import { IoText } from 'react-icons/io5';
+import { IoTextSharp } from 'react-icons/io5';
+
+//pins
+import { IoMdPin } from 'react-icons/io';
+import { MdLoationPin } from 'react-icons/md';
+import { LuMapPin } from 'react-icons/lu';
+
+//arrows (eg for exits, or pins)
+import { FaArrowLeft, FaArrowRight, FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import { FaLongArrowAltLeft, FaLongArrowAltRight, FaLongArrowAltUp, FaLongArrowAltDown } from 'react-icons/fa';
+
+//import {
+//    BiMaleFemale, BiFemale, BiMale,
+//} from 'react-icons/bi';
+//import {
+// FaFemale, FaMale, 
+//} from 'react-icons/fa';
+//import {
+//} from 'react-icons/fd';
+//import {
+//} from 'react-icons/gi';
+//import {
+//} from 'react-icons/gr';
+//    GrCafeteria, 
+//import {
+//} from 'react-icons/im';
+//import {
+//    IoCafe, 
+//} from 'react-icons/io';
+//import {
+//    LuGlassWater, 
+//} from 'react-icons/lu';
+//import {
+//MdWaterDrop, GiVendingMachine, PiBowlFoodFill, PiBowlFoodDuotone, IoFastFood, MdEmojiFood, 
+//} from 'react-icons/md';
+//import {
+//} from 'react-icons/pi';
+//import {
+//} from 'react-icons/rx';
+
+export default function Top({ isPressed, setIsPressed, mode, setMode}) {
+    ////initialized mode
+    //room
+    //washroom: men, women, neutral
+    //
+    //seating area
+    //
+    //travel: elevator and stairs and exit
+    //
+    //food: fountain and vending machine and cafe
+    //
+    //elevator
+    //
+    //elevator and stairs
+    //stairs
+    //exit
+    //
+    //fountain
+    //seating area
+    //food area
+    //vending machine
+
+    //const buttons = [
+    //    {
+    //        name: "room",
+    //        icon: "",
+    //        label: "Room",
+    //        pressed: false,
+    //    },
+    //    {
+    //        name: "washroom",
+    //        icon: "",
+    //        label: "WC",
+    //        pressed: false,
+    //    },
+    //    {
+    //        name: "room",
+    //        icon: "",
+    //        label: "Room",
+    //        pressed: false,
+    //    },
+    //    {
+    //        name: "room",
+    //        icon: "",
+    //        label: "Room",
+    //        pressed: false,
+    //    },
+    //    {
+    //        name: "room",
+    //        icon: "",
+    //        label: "Room",
+    //        pressed: false,
+    //    },
+    //    {
+    //        name: "room",
+    //        icon: "",
+    //        label: "Room",
+    //        pressed: false,
+    //    },
+    //];
+    const initialMode = {};
+    //setMode(initialMode);
+
+
     const [tappedWhenPressed, setTappedWhenPressed] = useState(false);
     const handleTouchStart = (e) => {
         if (isPressed) {
@@ -24,9 +179,32 @@ export default function Top({ isPressed, setIsPressed }) {
             setIsPressed(false);
         }
     }
+
+    //setMode({test: "hello"});
+    const valuesForShow = [];
+    const valuesForAdd = [];
+
+    let amount = 5;
+    for (let i = 0; i < amount; i++) {
+        valuesForShow.push(false);
+        valuesForAdd.push(false);
+    }
+
+    //const handleClick = () => {
+        //setMode(
+        //    {
+        //        show:
+        //            valuesForShow,
+        //        add:
+        //            valuesForAdd,
+        //    }
+        //);
+    //};
+
     return (
         <div className='top prevent_select'>
             {/*
+        <div className='top prevent_select' onClick={handleClick}>
         <div className='top'>
         */}
             <div className="button_row">
@@ -59,19 +237,44 @@ export default function Top({ isPressed, setIsPressed }) {
                     Add
                 </button>
                 <ToggleButton className="toggle_button">
-                    RM
+                    {
+                    //room
+                    <IoTextSharp style={{pointerEvents: 'none'}}/>
+                    //"rooms"
+                    }
                 </ToggleButton>
                 <ToggleButton className="toggle_button">
-                    RM
+                    {
+                    //washroom
+                    <BiMaleFemale style={{pointerEvents: 'none'}}/>
+                    //"washrooms"
+                    }
                 </ToggleButton>
                 <ToggleButton className="toggle_button">
-                    WC
+                    {
+                    //food
+                    <IoCafe style={{pointerEvents: 'none'}}/>
+                    //"food etc."
+                    }
                 </ToggleButton>
                 <ToggleButton className="toggle_button">
-                    S/E
+                    {
+                    //stairs, elevators, exits
+                    <FaStairs style={{pointerEvents: 'none'}}/>
+                    //"stairs etc."
+                    //<GiStairs style={{pointerEvents: 'none'}}/>
+                    //<MdOutlineStairs style={{pointerEvents: 'none'}}/>
+                    }
                 </ToggleButton>
                 <ToggleButton className="toggle_button last_button">
-                    FTN
+                    {
+                    //room
+                    <MdChair style={{pointerEvents: 'none'}}/>
+                    //"seating"
+                    //<MdOutlineChair style={{pointerEvents: 'none'}}/>
+                    //<PiChairBold style={{pointerEvents: 'none'}}/>
+                    //<PiChair style={{pointerEvents: 'none'}}/>
+                    }
                 </ToggleButton>
             </div>
         </div>
